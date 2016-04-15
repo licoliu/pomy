e92cad2102d3fb7cd10f51b305f5b81e7a7356c2
lico <lico.atom@gmail.com>
2016-04-15 16:04:07 +0800

    To name the package exclude version.

--- tasks/lifecycle/default/package.js
+++ tasks/lifecycle/default/package.js
@@ -4,12 +4,12 @@ var
   gulp = require('gulp'),
   zip = require('gulp-zip'),
   name = global.settings.name,
-  version = global.settings.version,
+  // version = global.settings.version,
   settings = global.settings,
   target = settings.target;
 
 gulp.task('package', ['prepare-package'], function() {
   return gulp.src(target.classes + "/**/*")
-    .pipe(zip(name + "@" + version + '.zip'))
+    .pipe(zip(name /*+ "@" + version*/ + '.zip'))
     .pipe(gulp.dest(target.root));
 });
