3580acc52f720c9e4bd5756a53b3ba1d6d6927f9
lico <lico.atom@gmail.com>
2016-05-09 14:07:09 +0800

    Repack all dependencies.

--- tasks/lifecycle/default/process-classes.js
+++ tasks/lifecycle/default/process-classes.js
@@ -13,70 +13,6 @@ var
   //browserSync = require('browser-sync'),
   dest = global.settings.dest;
 
-gulp.task("repack-mousewheel", function() {
-  var root = global.getRootPath();
-  return gulp.src([
-      root + dest.lib + "/jquery-mousewheel/js/jquery.mousewheel.js"
-    ])
-    .pipe(rename("jquery-mousewheel.js"))
-    .pipe(gulp.dest(root + dest.lib + "/jquery-mousewheel/js/"));
-});
-
-gulp.task("repack-malihu-css", function() {
-  var root = global.getRootPath();
-  return gulp.src([
-      root + dest.lib + "/malihu-custom-scrollbar-plugin/css/jquery.mCustomScrollbar.css"
-    ])
-    .pipe(rename("jquery-mCustomScrollbar.css"))
-    .pipe(gulp.dest(root + dest.lib + "/malihu-custom-scrollbar-plugin/css/"));
-});
-
-gulp.task("repack-malihu", ["repack-malihu-css"], function() {
-  var root = global.getRootPath();
-  return gulp.src([
-      root + dest.lib + "/malihu-custom-scrollbar-plugin/js/jquery.mCustomScrollbar.js"
-    ])
-    .pipe(rename("jquery-mCustomScrollbar.js"))
-    .pipe(gulp.dest(root + dest.lib + "/malihu-custom-scrollbar-plugin/js/"));
-});
-
-gulp.task("repack-ionrangeslider-skin", function() {
-  var root = global.getRootPath();
-  return gulp.src([
-      root + dest.lib + "/ionrangeslider/css/ion.rangeSlider.skinNice.css"
-    ])
-    .pipe(rename("ion-rangeSlider-skinNice.css"))
-    .pipe(gulp.dest(root + dest.lib + "/ionrangeslider/css/"));
-});
-
-gulp.task("repack-ionrangeslider-css", ["repack-ionrangeslider-skin"], function() {
-  var root = global.getRootPath();
-  return gulp.src([
-      root + dest.lib + "/ionrangeslider/css/ion.rangeSlider.css"
-    ])
-    .pipe(rename("ion-rangeSlider.css"))
-    .pipe(gulp.dest(root + dest.lib + "/ionrangeslider/css/"));
-});
-
-gulp.task("repack-ionrangeslider", ["repack-ionrangeslider-css"], function() {
-  var root = global.getRootPath();
-  return gulp.src([
-      root + dest.lib + "/ionrangeslider/js/ion.rangeSlider.js"
-    ])
-    .pipe(rename("ion-rangeSlider.js"))
-    .pipe(gulp.dest(root + dest.lib + "/ionrangeslider/js/"));
-});
-
-gulp.task("repack-angular-strap-tpl", function() {
-  var root = global.getRootPath();
-  var pomy = global.getPomyPath();
-  return gulp.src([
-      pomy + "bower_components/angular-strap/dist/angular-strap.tpl.js"
-    ])
-    .pipe(rename("angular-strap-tpl.js"))
-    .pipe(gulp.dest(root + dest.lib + "/angular-strap/js/"));
-});
-
 gulp.task("repack-bootstrap-woff2", function() {
   var root = global.getRootPath();
   var pomy = global.getPomyPath();
@@ -84,23 +20,6 @@ gulp.task("repack-bootstrap-woff2", function() {
       pomy + "bower_components/bootstrap/fonts/glyphicons-halflings-regular.woff2"
     ])
     .pipe(gulp.dest(root + dest.lib + "/bootstrap/fonts/"));
-});
-
-gulp.task("repack-easing", function() {
-  var root = global.getRootPath();
-  return gulp.src([
-      root + dest.lib + "/jquery-easing-original/js/jquery.easing.js"
-    ])
-    .pipe(rename("jquery-easing.js"))
-    .pipe(gulp.dest(root + dest.lib + "/jquery-easing-original/js/"));
-});
-
-gulp.task("repack-animate-css", function() {
-  var root = global.getRootPath();
-  return gulp.src([
-      root + dest.lib + "/animate.css/css/**/*"
-    ])
-    .pipe(gulp.dest(root + dest.lib + "/animate/css/"));
 });
 
 gulp.task("repack-iCheck-png", function() {
@@ -121,23 +40,6 @@ gulp.task("repack-iCheck-css", ["repack-iCheck-png"], function() {
     .pipe(gulp.dest(root + dest.lib + "/iCheck/skins/square/"));
 });
 
-gulp.task("repack-iCheck", ["repack-iCheck-css"], function() {
-  var root = global.getRootPath();
-  return gulp.src([
-      root + dest.lib + "/iCheck/js/icheck.min.js"
-    ])
-    .pipe(rename("iCheck.js"))
-    .pipe(gulp.dest(root + dest.lib + "/iCheck/js/"));
-});
-gulp.task("repack-angular-ui-tree", function() {
-  var root = global.getRootPath();
-  return gulp.src([
-      root + dest.lib + "/angular-ui-tree/css/angular-ui-tree.min.css"
-    ])
-    .pipe(rename("angular-ui-tree.css"))
-    .pipe(gulp.dest(root + dest.lib + "/angular-ui-tree/css/"));
-});
-
 gulp.task("repack-angular-ui-css", function() {
   var root = global.getRootPath();
   var pomy = global.getPomyPath();
@@ -146,6 +48,7 @@ gulp.task("repack-angular-ui-css", function() {
     ])
     .pipe(gulp.dest(root + dest.lib + "/angular-ui/css/"));
 });
+
 gulp.task("repack-angular-ui", ["repack-angular-ui-css"], function() {
   var root = global.getRootPath();
   var pomy = global.getPomyPath();
@@ -155,26 +58,7 @@ gulp.task("repack-angular-ui", ["repack-angular-ui-css"], function() {
     .pipe(gulp.dest(root + dest.lib + "/angular-ui/js/"));
 });
 
-gulp.task("repack-ng-table-css", function() {
-  var root = global.getRootPath();
-  var pomy = global.getPomyPath();
-  return gulp.src([
-      root + dest.lib + "/ng-table/css/ng-table.min.css"
-    ])
-    .pipe(rename("ng-table.css"))
-    .pipe(gulp.dest(root + dest.lib + "/ng-table/css/"));
-});
-gulp.task("repack-ng-table", ["repack-ng-table-css"], function() {
-  var root = global.getRootPath();
-  var pomy = global.getPomyPath();
-  return gulp.src([
-      root + dest.lib + "/ng-table/js/ng-table.min.js"
-    ])
-    .pipe(rename("ng-table.js"))
-    .pipe(gulp.dest(root + dest.lib + "/ng-table/js/"));
-});
-
-gulp.task("repack-google-code-prettify-styles", function() {
+gulp.task("repack-google-code-prettify", function() {
   var root = global.getRootPath();
   var pomy = global.getPomyPath();
   return gulp.src([
@@ -185,56 +69,17 @@ gulp.task("repack-google-code-prettify-styles", function() {
     ])
     .pipe(gulp.dest(root + dest.lib + "/google-code-prettify/css/"));
 });
-gulp.task("repack-google-code-prettify-css", ["repack-google-code-prettify-styles"], function() {
-  var root = global.getRootPath();
-  var pomy = global.getPomyPath();
-  return gulp.src([
-      root + dest.lib + "/google-code-prettify/css/prettify.min.css"
-    ])
-    .pipe(rename("prettify.css"))
-    .pipe(gulp.dest(root + dest.lib + "/google-code-prettify/css/"));
-});
-gulp.task("repack-google-code-prettify", ["repack-google-code-prettify-css"], function() {
-  var root = global.getRootPath();
-  var pomy = global.getPomyPath();
-  return gulp.src([
-      root + dest.lib + "/google-code-prettify/js/prettify.min.js"
-    ])
-    .pipe(rename("prettify.js"))
-    .pipe(gulp.dest(root + dest.lib + "/google-code-prettify/js/"));
-});
 
-gulp.task("repack-file", [
-  "repack-mousewheel",
-  "repack-malihu",
-  "repack-ionrangeslider",
-  "repack-angular-strap-tpl",
+gulp.task("repack-specs", [
   "repack-bootstrap-woff2",
-  "repack-animate-css",
-  "repack-iCheck",
-  "repack-angular-ui-tree",
+  "repack-iCheck-css",
   "repack-angular-ui",
-  "repack-easing",
-  "repack-ng-table",
   "repack-google-code-prettify"
 ], function(cb) {
   var root = global.getRootPath();
   del([
-    root + dest.lib + "/jquery-mousewheel/js/jquery.mousewheel.js",
-    root + dest.lib + "/malihu-custom-scrollbar-plugin/js/jquery.mCustomScrollbar.js",
-    root + dest.lib + "/malihu-custom-scrollbar-plugin/css/jquery.mCustomScrollbar.css",
-    root + dest.lib + "/ionrangeslider/js/ion.rangeSlider.js",
-    root + dest.lib + "/ionrangeslider/css/ion.rangeSlider.css",
-    root + dest.lib + "/ionrangeslider/css/ion.rangeSlider.skinNice.css",
     root + dest.lib + "/highstock-release/js/exporting.js",
-    root + dest.lib + "/highstock-release/js/highcharts-more.js",
-    root + dest.lib + "/jquery-easing-original/js/jquery.easing.js",
-    root + dest.lib + "/animate.css",
-    root + dest.lib + "/iCheck/js/icheck.min.js",
-    root + dest.lib + "/ng-table/css/ng-table.min.css",
-    root + dest.lib + "/ng-table/js/ng-table.min.js",
-    root + dest.lib + "/google-code-prettify/css/prettify.min.css",
-    root + dest.lib + "/google-code-prettify/js/prettify.min.js"
+    root + dest.lib + "/highstock-release/js/highcharts-more.js"
   ], {
     force: true
   }).then(function(deletedFiles) {
@@ -244,9 +89,55 @@ gulp.task("repack-file", [
   });
 });
 
-gulp.task("repack-jslib", ["repack-file"], function() {
+gulp.task("repack-folders-rename", ["repack-specs"], function() {
   var root = global.getRootPath();
-  return gulp.src(root + dest.lib + '/**/*.js')
+  gulp.src([root + dest.lib + '/*.*/**/*'])
+    .pipe(rename(function(path) {
+      // path.dirname path.basename path.extname
+      path.dirname = path.dirname.replace(/[.]/g, "-");
+    }))
+    .pipe(gulp.dest(root + dest.lib));
+});
+
+gulp.task("repack-folders", ["repack-folders-rename"], function(cb) {
+  var root = global.getRootPath();
+  del([
+    root + dest.lib + "/*.*"
+  ], {
+    force: true
+  }).then(function(deletedFiles) {
+    cb();
+  }, function(err) {
+    cb(err);
+  });
+});
+
+gulp.task("repack-files-rename", ["repack-folders"], function() {
+  var root = global.getRootPath();
+  gulp.src([root + dest.lib + '/**/*.js', root + dest.lib + '/**/*.css'])
+    .pipe(rename(function(path) {
+      // path.dirname path.basename path.extname
+      path.basename = path.basename
+        .replace(/(.min|.debug|.all)$/, "")
+        .replace(/[.]/g, "-");
+    }))
+    .pipe(gulp.dest(root + dest.lib));
+});
+
+gulp.task("minify-csslib", ["repack-files-rename"], function() {
+  var root = global.getRootPath();
+  return gulp.src([root + dest.lib + '/**/*.css',
+      "!" + root + dest.lib + "/**/*.*.css"
+    ])
+    .pipe(gulpif(!global.settings.debug, minifyCss()))
+    .pipe(gulp.dest(root + dest.lib));
+});
+
+gulp.task("minify-jslib", ["minify-csslib"], function() {
+  var root = global.getRootPath();
+  return gulp.src([root + dest.lib + '/**/*.js',
+      "!" + root + dest.lib + "/**/*.*.js"
+    ])
     .pipe(gulpif(!global.settings.debug, uglify({
       mangle: {
         except: ['require', 'exports', 'module']
@@ -255,14 +146,7 @@ gulp.task("repack-jslib", ["repack-file"], function() {
     .pipe(gulp.dest(root + dest.lib));
 });
 
-gulp.task("repack-csslib", ["repack-file"], function() {
-  var root = global.getRootPath();
-  return gulp.src(root + dest.lib + '/**/*.css')
-    .pipe(gulpif(!global.settings.debug, minifyCss()))
-    .pipe(gulp.dest(root + dest.lib));
-});
-
-gulp.task("define", ["repack-csslib", "repack-jslib"], function() {
+gulp.task("define", ["minify-jslib"], function() {
   var before = '',
     after = '';
   var root = global.getRootPath();
@@ -285,8 +169,22 @@ gulp.task("define", ["repack-csslib", "repack-jslib"], function() {
   //}));
 });
 
+gulp.task("repack", ["define"], function(cb) {
+  var root = global.getRootPath();
+  del([
+    root + dest.lib + "/**/*.*.js",
+    root + dest.lib + "/**/*.*.css"
+  ], {
+    force: true
+  }).then(function(deletedFiles) {
+    cb();
+  }, function(err) {
+    cb(err);
+  });
+});
+
 gulp.task('process-classes', ['compile'], function(cb) {
-  exec(global.getCommandPath('gulp') + ' define --process child', {
+  exec(global.getCommandPath('gulp') + ' repack --process child', {
     cwd: global.settings.cwd
   }, function(err, stdout, stderr) {
     console.log(stdout);
